acafb6630b4a48826e57eae78fc3228d36270e8b
louis91330 <devillepinlouis@gmail.com>
2018-05-03 16:17:17 +0200

    modif louis

---
+++ PROJET/Projet piscine/emploi.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Proposition d'emplois</title>
+</head>
+<body>
+
+</body>
+</html>
--- PROJET/inscription.php
+++ PROJET/inscription.php
@@ -1,35 +1,4 @@
-
-<!DOCTYPEhtml>
-<html>
-  <head>
-    <title>Inscription à Lindedin</title>
-    <link href="projet1.css" rel="stylesheet" type="text/css">
-    <meta charset="utf-8" />
-  </head> 
-
-  <body>
-
-    <h2 action="connexion.php" id="page-heading1"  class="titres">Inscription à Lindedin</h1>
-
-    <form id="page-form1" class="list" method="post" >
-
-          <label class="item item-input" id="page-input5">
-        <span class="input-label">Entrer votre adresse mail</span>
-        <input type="text" name="Mail" placeholder="">
-      </label><br />
-
-      <label class="item item-input" id="page-input3">
-        <span class="input-label">Entrez votre nouveau mot de passe</span>
-        <input type="password" name="mdp" placeholder="">
-      </label><br />
-
-      <label class="item item-input" id="page-input4">
-        <span class="input-label">Confirmer votre mot de passe</span>
-        <input type="password" name="cpassword" placeholder="">
-      </label><br /><br />
-      <input type="submit" value="Valider">
-
-    </form>
+<meta charset="utf-8">
 
   <?php
 	try
@@ -40,14 +9,34 @@
 	{
 	        die('Erreur : ' . $e->getMessage());
 	}
-
+    $Mail="";
+    $mdp="";
+    $cmdp="";
+    $Nom="";
     $Mail= $_POST['Mail'];
     $mdp= $_POST['mdp'];
+    $cmdp= $_POST['cmdp'];
+    $Nom = $_POST['Nom'];
 
-	$req = $bdd->prepare('INSERT INTO membre (Mail, mdp) VALUES(:Mail,:mdp)');
-	$req->execute(array(
-	  'Mail' => $Mail,
-  	  'mdp' => $mdp
-	  ));
+
+if($cmdp!=$mdp)
+{
+echo "Le mot de passe et sa confirmation ne sont pas identiques.Veuillez réessayer.";
+}
+if(($Mail=="")||($Nom=="")||($mdp=="")||($cmdp==""))
+{
+  echo "Un champ est vide. Veuillez réessayer.";
+}
+
+if(($Mail!="")&&($Nom!="")&&($mdp!="")&&($cmdp!="")&&($cmdp==$mdp))
+{
+    $req = $bdd->prepare('INSERT INTO membre (Mail,Nom, mdp) VALUES(:Mail,:Nom,:mdp)');
+    $req->execute(array('Mail' => $Mail,'Nom' => $Nom,'mdp' => $mdp));
+    header('Location:connexion.html');
+}
+
+
+
+
 	?>
 
